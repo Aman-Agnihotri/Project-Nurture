24c7cec40312d2c6d87a81efa2e9bb700aeaf59a
Sachin <raisethebat2020@gmail.com>
2024-03-13 15:05:26 +0530

    initial front-end

---
+++ malnutrition/src/components/Home.jsx
@@ -0,0 +1,83 @@
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+import img1 from '../assets/img1.webp';
+import img2 from '../assets/img2.png';
+import img3 from '../assets/img3.webp';
+import img4 from '../assets/logo.jpeg';
+
+const headingOptions={
+
+  pos:"absolute",
+  left:"50%",
+  top:"40%",
+  transform:"translate(-50%,-50%)",
+  textTransform:"uppercase",
+  p:'4'
+}
+
+const Home = () => {
+  return (
+     <Box >
+ {/* w="80%" margin="0 auto" */}
+      <MyCarousel />
+
+        <Container
+          maxW={'container.xl'} maxH={'100vh'} p='16' 
+        >
+          <Heading 
+           textTransform={"uppercase"} 
+           py='2' w="fit-content" 
+           borderBottom={'5px solid teal'}
+           m='auto'
+          >
+            jkhlik</Heading>
+        </Container>
+
+      </Box>
+  );
+};
+
+const MyCarousel = () => (
+  <Carousel width={"100vh"}  height={"40vh"}
+    autoPlay={true}
+    infiniteLoop
+    interval={1000}
+    showStatus={false}
+    showThumbs={false}
+    >
+      
+    <Box w="full" h={'100vh'}>
+      <Image src={img4} h="full" w={['full','50%']}  objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.400'} color={'white'} {...headingOptions}>
+      </Heading>
+      </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img1} h="full" w={['full','50%']}  objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.400'} color={'white'} {...headingOptions}>
+      "Empty Bowl, Full Heart: A Cry for Compassion"
+      </Heading>
+      </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img2} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.500'} color={'black'} {...headingOptions}>
+      "Nutrient Deficiency Impacting Child Growth"
+      </Heading>
+
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img3} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+      "Unequal Childhoods: From Screens to Empty Stomachs"
+      </Heading>
+      <Heading>POWER</Heading>
+    </Box>
+  </Carousel>
+);
+
+export default Home;
