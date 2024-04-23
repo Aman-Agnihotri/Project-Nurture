cfee6b8246f1124148dc83b3dee403313635ddb6
Shivam Kumar <shivambspr@gmail.com>
2024-04-23 16:15:37 +0530

    Added the firebase config.

---
+++ malnutrition/src/lib/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7eTdjoIfAUTO4pBTjhCj0Np2h3bqPpJA",
+  authDomain: "project-nurture.firebaseapp.com",
+  projectId: "project-nurture",
+  storageBucket: "project-nurture.appspot.com",
+  messagingSenderId: "972337939607",
+  appId: "1:972337939607:web:afd880c4f21803ce0cd5d2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth()
+export const db = getFirestore()
