29e79491614984b95904bed3485f39f83e43d641
hunterhhh412 <amanagnihotri412002@gmail.com>
2024-04-24 23:37:03 +0530

    The heat map is working as it should, but the data points need to be improved.

--- project_nurture/src/components/MapComponent.jsx
+++ project_nurture/src/components/MapComponent.jsx
@@ -16,7 +16,7 @@ const MapComponent = () => {
         const tile = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 18,
-            minZoom: 3,
+            minZoom: 2,
             id: 'mapbox/streets-v11', // Example style
             tileSize: 512,
             zoomOffset: -1,
@@ -41,7 +41,7 @@ const MapComponent = () => {
 
                     const lat = row.Latitude;
                     const lon = row.Longitude;
-                    const scale = row.Scale;
+                    const scale = row.Scale/10;
 
                     coordinates.push([lat, lon, scale]);
 
