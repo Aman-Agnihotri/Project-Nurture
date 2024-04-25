b9ceb0414e3a23ef7c165906c5b1949c5ee23c01
Sachin <raisethebat2020@gmail.com>
2024-04-25 20:51:33 +0530

    added notification

--- project_nurture/src/App.jsx
+++ project_nurture/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from './components/Footer';
@@ -7,21 +7,25 @@ import Signup from './components/Signup.jsx';
 import About from './components/About.jsx';
 import Form from './components/Form.jsx';
 import Dashboard from './components/Dashboard.jsx';
+import Notification from './components/Notification.jsx'; // Import the Notification component
 
 function App() {
   return (
-    <Router>
-     <Header/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/form" element={<Form />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/about" element={<About />} />
-    </Routes>
-      <Footer/>
-    </Router>
+    <>
+      <Notification /> {/* Use the Notification component here */}
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/form" element={<Form />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
--- project_nurture/src/components/Login.jsx
+++ project_nurture/src/components/Login.jsx
@@ -1,87 +1,13 @@
-// import {
-//   Button,
-//   Container,
-//   Heading,
-//   Input,
-//   Text,
-//   VStack,
-// } from '@chakra-ui/react';
-// import { Link, useNavigate } from 'react-router-dom';
-// import { toast } from "react-toastify";
-// import { signInWithEmailAndPassword } from "firebase/auth";
-// import { auth } from "../lib/firebase";
-
-// const Login = () => {
-//   const navigate = useNavigate();
-
-//   const handleLogin = async (event) => {
-//     event.preventDefault();
-
-//     const formData = new FormData(event.target);
-//     const { email, password } = Object.fromEntries(formData);
-
-//     try {
-//       await signInWithEmailAndPassword(auth, email, password);
-//       console.log('Login successful');
-//       navigate('/dashboard');
-//       } catch (err) {
-//       // toast.err(err.message);
-//       console.log(err);
-//     }
-//   };
-
-//   return (
-//     <Container maxW={'container.xl'} h={'100vh'} p={'16'}>
-//       <form onSubmit={handleLogin}>
-//         <VStack
-//           alignItems={'stretch'}
-//           spacing={'8'}
-//           w={['full', '96']}
-//           m={'auto'}
-//           my={'16'}
-//         >
-//           <Heading>Welcome Back</Heading>
-
-//           <Input
-//             placeholder={'Email Address'}
-//             type={'text'}
-//             required = {true}
-//             focusBorderColor={' teal.500'}
-//             name= {"email"}
-//           />
-//           <Input
-//             placeholder={'Password'}
-//             type={'password'}
-//             required = {true}
-//             focusBorderColor={' teal.500'}
-//             name={"password"}
-//           />
-
-//           <Button variant={'link'} alignSelf={'flex-end'}>
-//             <Link to={'/forgetpassword'}>Forget Password?</Link>
-//           </Button>
-
-//           <Button colorScheme={' teal'} type={'submit'}>
-//             Log In
-//           </Button>
-
-//           <Text textAlign={'right'}>
-//             New User?{' '}
-//             <Button variant={'link'} colorScheme={' teal'}>
-//               <Link to={'/signup'}>Sign Up</Link>
-//             </Button>
-//           </Text>
-//         </VStack>
-//       </form>
-//     </Container>
-//   );
-// };
-
-// export default Login;
-
-import { Button, Container, Heading, Input, Text, VStack } from '@chakra-ui/react';
+import {
+  Button,
+  Container,
+  Heading,
+  Input,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
 import { Link, useNavigate } from 'react-router-dom';
-import { toast } from "react-toastify"; // Import react-toastify
+import { toast } from "react-toastify";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../lib/firebase";
 
@@ -98,8 +24,8 @@ const Login = () => {
       await signInWithEmailAndPassword(auth, email, password);
       console.log('Login successful');
       navigate('/dashboard');
-    } catch (err) {
-      toast.error(err.message); // Use toast.error to display error message
+      } catch (err) {
+      // toast.err(err.message);
       console.log(err);
     }
   };
@@ -119,15 +45,15 @@ const Login = () => {
           <Input
             placeholder={'Email Address'}
             type={'text'}
-            required={true}
-            focusBorderColor={'teal.500'}
-            name={"email"}
+            required = {true}
+            focusBorderColor={' teal.500'}
+            name= {"email"}
           />
           <Input
             placeholder={'Password'}
             type={'password'}
-            required={true}
-            focusBorderColor={'teal.500'}
+            required = {true}
+            focusBorderColor={' teal.500'}
             name={"password"}
           />
 
@@ -135,13 +61,13 @@ const Login = () => {
             <Link to={'/forgetpassword'}>Forget Password?</Link>
           </Button>
 
-          <Button colorScheme={'teal'} type={'submit'}>
+          <Button colorScheme={' teal'} type={'submit'}>
             Log In
           </Button>
 
           <Text textAlign={'right'}>
             New User?{' '}
-            <Button variant={'link'} colorScheme={'teal'}>
+            <Button variant={'link'} colorScheme={' teal'}>
               <Link to={'/signup'}>Sign Up</Link>
             </Button>
           </Text>
