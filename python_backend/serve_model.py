cbb22b308b9aa1010e56b225660f3ed0f3c38721
hunterhhh412 <amanagnihotri412002@gmail.com>
2024-05-03 06:09:03 +0530

    Added the CORS support in the flask server.

--- python_backend/serve_model.py
+++ python_backend/serve_model.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
+from flask_cors import CORS
 import joblib
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)
 
 # Load the model
 model = joblib.load('random_forest_model.pkl')
